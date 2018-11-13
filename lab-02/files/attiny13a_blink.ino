dece91cc12c026a2e0e40c6ca86bb34a4fa5ec9e
kiwicon-badge <43776003+kiwicon-badge@users.noreply.github.com>
2018-11-13 23:11:39 +1300

    Changed to support two pins.

--- lab-02/files/attiny13a_blink.ino
+++ lab-02/files/attiny13a_blink.ino
@@ -1,10 +1,14 @@
 void setup() {
-  //The 8-pin ATTinys has 5 usable digital output pins 0-4
+  // The 8-pin ATTinys has 5 usable digital output pins 0-4
+  // These can be set to input, output, or high impedeance
   pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
 }
 void loop() {
-  digitalWrite(0, HIGH);    //Set the LED pins to HIGH. This gives power to the LED and turns it on
-  delay(500);   // Wait for a half a second
-  digitalWrite(0, LOW);   // Set the LED pins to LOW. This turns it off
-  delay(1000);    // Wait for a half second
+  digitalWrite(0, HIGH); // Set pin 0 to HIGH. This gives power to the LED and turns it on
+  delay(500);            // Wait for a half a second
+  digitalWrite(0, LOW);   // Set pin 0 to LOW. This turns it off
+  digitalWrite(1, HIGH); // Set pin 1 to HIGH. This gives power to the LED and turns it on
+  delay(500);            // Wait for a half a second
+  digitalWrite(1, LOW);   // Set pin 1 to LOW. This turns it off
 }
